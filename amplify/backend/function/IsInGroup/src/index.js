e7bb701044ec3ab4ce54d16300d6b24091ce8e3c
dudi <neemandu@gmail.com>
2021-01-12 09:39:22 +0200

    fix getgroup

--- amplify/backend/function/IsInGroup/src/index.js
+++ amplify/backend/function/IsInGroup/src/index.js
@@ -2,6 +2,8 @@
 	API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
 	API_CARDSPACKS_GROUPTABLE_ARN
 	API_CARDSPACKS_GROUPTABLE_NAME
+	API_CARDSPACKS_USERTABLE_ARN
+	API_CARDSPACKS_USERTABLE_NAME
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
@@ -40,22 +42,58 @@ async function getGroup(groupId){
     return group;
 }
 
+async function getUserByUSerName(username){
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var userTable = env.API_CARDSPACKS_USERTABLE_NAME;
+
+    
+    var userParams = {
+        TableName:userTable,
+        Key:{
+            "id": username
+        }
+    };
+
+    console.log("searching for user - " + username);
+
+    var user;
+
+    await docClient.get(userParams, function(err, data) {
+        if (err) {
+            console.log("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Get user succeeded:", JSON.stringify(data, null, 2));
+            user = data["Item"];
+        }
+    }).promise();
+
+    if(!user){
+        throw Error ('no such user - ' + username);
+    }
+
+    return user;
+
+}
+
 exports.handler = async (event) => {
     AWS.config.update({
         region: env.REGION
         //endpoint: env.API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
     });
 
-    var email = event.identity.claims['cognito:email'];
-    if(!email){
-        email = event.identity.claims['email'];
+    var username = event.identity.claims['cognito:username'];
+    if(!username){
+        username = event.identity.claims['username'];
     }
     var args = event.arguments.input;
     var group = await getGroup(args['id']);
 
+    var user = await getUserByUSerName(username);
     for(var i = 0; i < group.groupUsers.length ; i++){
         var curremail = group.groupUsers[i].email;
-        if(email == curremail){
+        if(user.email == curremail){
             return true;
         }
     }
--- amplify/backend/function/getGroup/src/index.js
+++ amplify/backend/function/getGroup/src/index.js
@@ -2,6 +2,8 @@
 	API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
 	API_CARDSPACKS_GROUPTABLE_ARN
 	API_CARDSPACKS_GROUPTABLE_NAME
+	API_CARDSPACKS_USERTABLE_ARN
+	API_CARDSPACKS_USERTABLE_NAME
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
@@ -52,26 +54,62 @@ function canUserGetGroupDetails(email, group){
     return canRead;
 }
 
+async function getUserByUSerName(username){
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var userTable = env.API_CARDSPACKS_USERTABLE_NAME;
+
+    
+    var userParams = {
+        TableName:userTable,
+        Key:{
+            "id": username
+        }
+    };
+
+    console.log("searching for user - " + username);
+
+    var user;
+
+    await docClient.get(userParams, function(err, data) {
+        if (err) {
+            console.log("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Get user succeeded:", JSON.stringify(data, null, 2));
+            user = data["Item"];
+        }
+    }).promise();
+
+    if(!user){
+        throw Error ('no such user - ' + username);
+    }
+
+    return user;
+
+}
+
 exports.handler = async (event) => {
     AWS.config.update({
         region: env.REGION
         //endpoint: env.API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
     });
 
-    var email = event.identity.claims['cognito:email'];
-    if(!email){
-        email = event.identity.claims['email'];
+    var username = event.identity.claims['cognito:username'];
+    if(!username){
+        username = event.identity.claims['username'];
     }
 
+    var user = await getUserByUSerName(username);
     var groupId = event.arguments.input["id"];
     var group = await getGroup(groupId);
-    var canRead = canUserGetGroupDetails(email, group);
+    var canRead = canUserGetGroupDetails(user.email, group);
 
     if(canRead){
-        console.log('user: ' + email + ' is authorized to read the group details because he is an admin.' );
+        console.log('user: ' + username + ' is authorized to read the group details because he is an admin.' );
         return group;
     }
-    console.log('user: ' + email + ' is not authorized to read the group details.' );
+    console.log('user: ' + username + ' is not authorized to read the group details.' );
     return null;
 
 };
