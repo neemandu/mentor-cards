898c83d930b4ad51d0a47391590cbd9a4f993d4d
neemandu <neemandu@gmail.com>
2021-11-07 22:56:00 +0200

    trying to fix double token request from google

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ import AWSConfig from './aws-exports';
 Auth.configure(AWSConfig);*/
 // End Amplify Configuration
 
-/*
+
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
 Amplify.configure(aws_exports);
-*/
+
 
 if (environment.production) {
   enableProdMode();
