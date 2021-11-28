1f900f8a12e62230aa36cfae7a7e909f82e72763
yknobel <yknobel@YKNOBEL-MOBL1>
2021-11-28 14:08:59 +0200

    add design to all packs page

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -34,13 +34,13 @@ export class AuthService {
         this._authState.next(payload.event);
       }
     });
-    (Auth as any)._handleAuthResponse(this.authResponseUri)
-      .then(res => {
-        console.log("file: auth.service.ts ~ line 38 ~ constructor ~ res", res)
-      })
-      .err(err => {
-        console.log("file: auth.service.ts ~ line 44 ~ constructor ~ err", err)
-      })
+    // (Auth as any)._handleAuthResponse(this.authResponseUri)
+    //   .then(res => {
+    //     console.log("file: auth.service.ts ~ line 38 ~ constructor ~ res", res)
+    //   })
+    //   .err(err => {
+    //     console.log("file: auth.service.ts ~ line 44 ~ constructor ~ err", err)
+    //   })
   }
 
   signUp(user: NewUser): Promise<CognitoUser | any> {
