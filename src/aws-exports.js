beb395eb2bac3e28edf35df44f8ad3bd012cffc4
neemandu <neemandu@gmail.com>
2021-12-20 21:39:57 +0200

    aws

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,9 +8,10 @@ const awsmobile = {
     "aws_user_pools_id": "eu-west-2_s4Ax8bC3q",
     "aws_user_pools_web_client_id": "43nsctsqn9cjhl819dfbf2m329",
     "oauth": {},
-    "aws_cognito_login_mechanisms": [
+    "aws_cognito_username_attributes": [
         "EMAIL"
     ],
+    "aws_cognito_social_providers": [],
     "aws_cognito_signup_attributes": [
         "EMAIL"
     ],
@@ -35,7 +36,10 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://wvsvp4e2j5bodmuxb22veyd7vu.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_appsync_apiKey": "da2-mqmnvmgrrzgsjoxpjgev5ir3oy"
+    "aws_appsync_apiKey": "da2-mqmnvmgrrzgsjoxpjgev5ir3oy",
+    "aws_cognito_login_mechanisms": [
+        "EMAIL"
+    ]
 };
 
 
