ea9a2b9cc7fc2fd1c2060c0ce08abbf0077b74f1
neemandu <neemandu@gmail.com>
2022-02-21 21:48:10 +0200

    fix getendoftrialdate

--- amplify/backend/function/GetEndOfTrialDate/src/index.js
+++ amplify/backend/function/GetEndOfTrialDate/src/index.js
@@ -4,6 +4,8 @@ exports.handler = async (event) => {
     console.log('event.source');
     console.log(event.source);
     var startFreePeriodDate = event.source['createdAt'];
+    console.log('startFreePeriodDate');
+    console.log(startFreePeriodDate);
     var couponCodes = event.source['couponCodes']
     console.log('couponCodes');
     console.log(couponCodes);
@@ -11,15 +13,24 @@ exports.handler = async (event) => {
         couponCodes.length > 0){
             for(var i = 0 ; i < couponCodes.length ; i++){ 
                 if((!couponCodes[i].allowedCardsPacks) || (couponCodes[i].allowedCardsPacks.length == 0)){
+                    console.log('couponCodes[i]');
+                    console.log(couponCodes[i]);
                     var couponDate = couponCodes[i].createdAt;
                     if(couponDate > startFreePeriodDate){
+                        console.log('couponDate > startFreePeriodDate');
                         startFreePeriodDate = couponDate;
+                        console.log('startFreePeriodDate');
+                        console.log(startFreePeriodDate);
                         trialPeriodInDays = couponCodes[i].trialPeriodInDays;
+                        console.log('trialPeriodInDays');
+                        console.log(trialPeriodInDays);
                     }
                 }
             }
         }
-    
-    allPackagesDate.setDate(allPackagesDate.getDate()-trialPeriodInDays);   
-    return new Date(allPackagesDate);
+    startFreePeriodDate = new Date(startFreePeriodDate);
+    startFreePeriodDate.setDate(startFreePeriodDate.getDate()+trialPeriodInDays); 
+    console.log('allPackagesDate');
+    console.log(startFreePeriodDate);  
+    return new Date(startFreePeriodDate);
 };
