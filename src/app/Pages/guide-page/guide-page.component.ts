fd0a4512d29a454139d61acd106da08103559ec7
Yaniv.Kn <yaniv.knobel@solaredge.com>
2022-05-15 10:44:02 +0300

    fix all guide videos and about video

--- src/app/Pages/guide-page/guide-page.component.ts
+++ src/app/Pages/guide-page/guide-page.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class GuidePageComponent implements OnInit {
   selectedBtn: number = 1;
   title: string = "מהות העבודה עם קלפים";
+  playerWidth: number;
+  playerHeight: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.playerWidth = window.innerWidth;
+    this.playerHeight = this.playerWidth / 1.78
   }
 
   selectedTopicChanged(index: number, title: string): void {
