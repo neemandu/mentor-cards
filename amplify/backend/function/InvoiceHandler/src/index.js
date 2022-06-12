34cdb0e4297b520732ee7b85c8be9eada931868c
Neeman, Dudi <dudi.neeman@intel.com>
2022-06-12 22:48:57 +0300

    add invoice to msg queue

--- amplify/backend/function/InvoiceHandler/src/index.js
+++ amplify/backend/function/InvoiceHandler/src/index.js
@@ -1,16 +1,5 @@
 /* Amplify Params - DO NOT EDIT
 	API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
-	API_CARDSPACKS_MESSAGEQUEUETABLE_ARN
-	API_CARDSPACKS_MESSAGEQUEUETABLE_NAME
-	API_CARDSPACKS_USERTABLE_ARN
-	API_CARDSPACKS_USERTABLE_NAME
-	API_RECEIPTSAPI_APIID
-	API_RECEIPTSAPI_APINAME
-	ENV
-	REGION
-	STORAGE_INVOICES_BUCKETNAME
-Amplify Params - DO NOT EDIT *//* Amplify Params - DO NOT EDIT
-	API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
 	API_CARDSPACKS_USERTABLE_ARN
 	API_CARDSPACKS_USERTABLE_NAME
 	API_RECEIPTSAPI_APIID
@@ -201,11 +190,44 @@ function createHtml(invoice){
   return html;
 }
 
-function sendInvoiceToEmail(email, fullName, html){
-  console.log("sendInvoiceToEmail:");
-  console.log("email: " + email);
-  console.log("fullName: " + fullName);
-  console.log("html: " + html);
+function sendInvoiceToEmail(invoice){
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var table = env.API_CARDSPACKS_MESSAGEQUEUETABLE_NAME;
+  var d = new Date();
+  var id = email + "_INVOICE" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate();
+  var params = {
+      TableName:table,
+      Item:{
+          "id": id,
+          "email": invoice.email,
+          "emailDeliveryTime": null,
+          "phone": null,
+          "smsDeliveryTime": null,
+          "emailTemplateId": 11,
+          "name": invoice.fullName,
+          "params": {
+            "email": invoice.email,
+            "fullName": invoice.fullName,
+            "customerAddress": invoice.customerAddress,
+            "date": invoice.date,
+            "businessName": invoice.businessName,
+            "businessPhoneNumber": invoice.businessPhoneNumber,
+            "businessAddress": invoice.businessAddress,
+            "businessWebsite": invoice.businessWebsite,
+            "invoiceType": invoice.invoiceType,       
+            "invoiceRunningId": invoice.invoiceRunningId,
+            "item": invoice.item,
+            "item_price": invoice.item_price,
+            "item_quantity": invoice.item_quantity
+          }
+      }
+  };
+
+  await docClient.put(params).promise().then(data => {
+      console.log("Added item to message queue item:", JSON.stringify(data, null, 2));
+  }).catch(err => {
+      console.error("Unable to add welcome message to: " + email + ". Error JSON:", JSON.stringify(err, null, 2));
+  });
 }
 
 exports.handler = (event) => {
@@ -222,10 +244,10 @@ exports.handler = (event) => {
         fullName: record.dynamodb.NewImage.fullName.S,
         customerAddress: record.dynamodb.NewImage.customerAddress.S,
         date: new Date(record.dynamodb.NewImage.date.S).toLocaleDateString(),
-        businessName: 'Mentor-Cards',
-        businessPhoneNumber: '0549139859',
-        businessAddress: 'Maglan 4, Meitar',
-        businessWebsite: '',
+        businessName: record.dynamodb.NewImage.businessName.S,
+        businessPhoneNumber: record.dynamodb.NewImage.businessPhoneNumber.S,
+        businessAddress: record.dynamodb.NewImage.businessAddress.S,
+        businessWebsite: record.dynamodb.NewImage.businessWebsite.S,
         invoiceType: record.dynamodb.NewImage.invoiceType.S
       };
       
@@ -245,8 +267,7 @@ exports.handler = (event) => {
           invoice.item = items[0].M.itemName.S;
           invoice.item_price = items[0].M.pricePerItem.N;
           invoice.item_quantity = items[0].M.numberOfItems.N;
-          invoice.html = createHtml(invoice);
-          sendInvoiceToEmail(invoice.email, invoice.fullName, invoice.html);
+          sendInvoiceToEmail(invoice);
         });
       });
       
