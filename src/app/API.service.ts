445d5fc0cc579f8203dcf70420498bfe0b48f1b2
dudi <neemandu@gmail.com>
2022-10-20 15:31:10 +0300

    fix

--- src/app/API.service.ts
+++ src/app/API.service.ts
@@ -4858,6 +4858,44 @@ export class APIService {
     )) as any;
     return <boolean | null>response.data.UpdatePaymentProgram;
   }
+  async GetSubscriptionPlansForOrgs(
+    input: userInput
+  ): Promise<Array<GetSubscriptionPlansMutation>> {
+    const statement = `mutation GetSubscriptionPlans($input: userInput!) {
+        GetSubscriptionPlans(input: $input) {
+          __typename
+          id
+          name
+          description
+          providerPlanId
+          numberOfUsers
+          numberOfCardPacks
+          billingCycleInMonths
+          fullPrice
+          discount
+          orgMembership {
+            __typename
+            id
+            name
+            trialPeriodInDays
+            numberOfallowedCardsPacks
+            createdAt
+            updatedAt
+          }
+          createdAt
+          updatedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {
+      input
+    };
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <Array<GetSubscriptionPlansMutation>>(
+      response.data.GetSubscriptionPlans
+    );
+  }
   async GetSubscriptionPlans(
     input: userInput
   ): Promise<Array<GetSubscriptionPlansMutation>> {
@@ -7793,6 +7831,8 @@ export class APIService {
             }
             brief
             likesCounter
+            isExternalPack
+            visitorsCounter
             createdAt
             updatedAt
           }
