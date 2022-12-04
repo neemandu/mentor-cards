aa12a35984d6dd807ea21df92dbc0b7a3132e2e2
dudi <neemandu@gmail.com>
2022-12-04 09:50:44 +0200

    aws

--- amplify/backend/function/GetProviderPlanId/src/index.js
+++ amplify/backend/function/GetProviderPlanId/src/index.js
@@ -5,101 +5,19 @@
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
-const { env } = require("process");
-var AWS = require("aws-sdk");
-
-async function getUser(username){
-    var docClient = new AWS.DynamoDB.DocumentClient();
-
-    var userTable = env.API_CARDSPACKS_USERTABLE_NAME;
-
-    
-    var userParams = {
-        TableName:userTable,
-        Key:{
-            "id": username
-        }
-    };
-
-    console.log("searching for user - " + username);
-
-    var user;
-
-    await docClient.get(userParams).promise().then(data => {
-        console.log("Get user succeeded:", JSON.stringify(data, null, 2));
-        user = data["Item"];
-    }).catch(err => {
-        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    });
-
-    if(!user){
-        throw Error ('no such user - ' + username);
-    }
-
-    return user;
-}
-
-function noPlan(plans){
-    for(var i =0 ;i < plans.length; i++){
-        if(plans[i].subscriptionProviderPlanId){
-            plans[i].subscriptionProviderPlanId = plans[i].providerPlanId;
-        }
-    }
-    return plans;
-}
-
-function plan(plans){
-    for(var i =0 ;i < plans.length; i++){
-        if(plans[i].subscriptionProviderPlanId){
-            plans[i].providerPlanId = plans[i].subscriptionProviderPlanId;
-        }
-    }
-    return plans;
-}
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log('Get Provider Plan ID');
-    console.log('event');
-    console.log(event);
-    var plans = event.source['subscriptionPlans'];
-    if(typeof plans === 'undefined'){
-        return [];
-    }
-    // user was not identified in cognito
-    if((!("identity" in event)) || (event.identity == null)){
-        return noPlan(plans);
-    }
-
-    if(event.identity == null){
-        return [];
-    }
-
-    AWS.config.update({
-        region: env.REGION
-        //endpoint: env.API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
-    }); 
-
-    var username = event.identity.claims['cognito:username'];
-    if(!username){
-        username = event.identity.claims['username'];
-    }
-
-    var user = await getUser(username);
-    if(!user){
-        console.log('no user - noPlan');
-        return noPlan(plans);
-    }
-    else{
-        if(user.status == 'PLAN'){
-            console.log('user has a plan');
-            return plan(plans);
-        }
-        else{
-            console.log('user does not have a plan');
-            return noPlan(plans);
-        }
-    }
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
 };
