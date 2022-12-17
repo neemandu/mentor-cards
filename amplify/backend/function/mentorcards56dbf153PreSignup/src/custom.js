72f494be7758ae9f369a041a9edec93a5ca7f76d
dudi <neemandu@gmail.com>
2022-12-17 17:10:25 +0200

    aws changes for google sign in

--- amplify/backend/function/mentorcards56dbf153PreSignup/src/custom.js
+++ amplify/backend/function/mentorcards56dbf153PreSignup/src/custom.js
@@ -1,8 +1,74 @@
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event, context) => {
-  console.log("event");
+var AWS = require("aws-sdk");
+
+const cognito = new AWS.CognitoIdentityServiceProvider();
+
+const knownProviderNames = {
+  google: 'Google',
+  facebook: 'Facebook'
+};
+
+async function  getProviderName(userPoolId, providerName) {
+  if (knownProviderNames[providerName]) {
+    return knownProviderNames[providerName];
+  }
+
+  const { Providers } = await cognito.listIdentityProviders({ UserPoolId: userPoolId }).promise();
+  for (const provider of Providers) {
+    if (provider.ProviderName.toLowerCase() === providerName.toLowerCase()) {
+      return provider.ProviderName;
+    }
+  }
+}
+
+async function tryMergeUserAccounts (event) {
   console.log(event);
-  return event
+  const { userPoolId, userName } = event;
+  const { email } = event.request.userAttributes;
+  const [provider, ...providerValues] = userName.split('_');
+  const providerValue = providerValues.join('_');
+
+  // merge social provider with existing cognito user by email
+  if (provider.length > 0 && providerValue.length > 0) {
+    const [{ Users }, providerName] = await Promise.all([
+      cognito
+        .listUsers({
+          UserPoolId: userPoolId,
+          AttributesToGet: ['email'],
+          Filter: `email = "${email}"`,
+          Limit: 1
+        })
+        .promise(),
+      getProviderName(userPoolId, provider)
+    ]);
+
+    if (providerName && Users.length > 0) {
+      for (const user of Users) {
+        await cognito
+          .adminLinkProviderForUser({
+            UserPoolId: userPoolId,
+            DestinationUser: {
+              ProviderName: 'Cognito',
+              ProviderAttributeValue: user.Username
+            },
+            SourceUser: {
+              ProviderName: providerName,
+              ProviderAttributeName: 'Cognito_Subject',
+              ProviderAttributeValue: providerValue
+            }
+          })
+          .promise();
+      }
+
+      // return true to indicate users were merged
+      return true;
+    }
+  }
+
+  return false;
+}
+
+exports.handler = async (event, _, callback) => {
+  // continue the flow only if did not link providers
+  const wereUsersMerged = await tryMergeUserAccounts(event);
+  return wereUsersMerged ? event : callback(null, event);
 };
