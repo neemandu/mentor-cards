9205f9916d34c4e79efefeadfa2c6ca9922ce2fc
dudi <neemandu@gmail.com>
2022-12-18 21:12:39 +0200

    guide book fixes + category sort nlp last

--- src/app/Services/cards.service.ts
+++ src/app/Services/cards.service.ts
@@ -29,7 +29,7 @@ export class CardsService {
   allPacks: PackContent[];
   allCategories: string[] = [];
   favorites: any[] = [];
-  categoriesOrder: string[] = ['קלפי תמונה', 'שיתופי פעולה', 'קלפי שאלות', 'קלפי חגים', 'קלפי מילה', 'קלפי תמונה + מילה', 'קלפי מסרים', 'קלפי ערכים', 'ערכות במתנה'];
+  categoriesOrder: string[] = ['קלפי תמונה', 'שיתופי פעולה', 'קלפי שאלות', 'קלפי חגים', 'קלפי מילה', 'קלפי תמונה + מילה', 'קלפי מסרים', 'קלפי ערכים', 'ערכות במתנה', 'קלפי NLP', 'NLP קלפי'];
 
   constructor(private http: HttpClient, public _snackBar: MatSnackBar, private userAuthService: UserAuthService,
     private overlaySpinnerService: OverlaySpinnerService, private api: APIService) {
