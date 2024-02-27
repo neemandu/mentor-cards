272f679ed4d80a93bdd9fe2e0c733d34fa03af41
dudi <neemandu@gmail.com>
2024-02-27 16:28:32 +0200

    add newaffiliate handler function

---
+++ amplify/backend/function/NewAffiliateHandler/src/index.js
@@ -0,0 +1,77 @@
+/* Amplify Params - DO NOT EDIT
+	API_CARDSPACKS_GRAPHQLAPIIDOUTPUT
+	API_CARDSPACKS_USERTABLE_ARN
+	API_CARDSPACKS_USERTABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+async function saveUser(user){
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  user.updatedAt = new Date().toISOString();
+  var userTable = env.API_CARDSPACKS_USERTABLE_NAME;
+  var updatedUserParams = {
+      TableName: userTable,
+      Item: user
+  };
+
+  console.log("updating user " + user.id );
+
+  await docClient.put(updatedUserParams).promise().then(data => {
+      console.log("updated user " + user.id, JSON.stringify(data, null, 2));
+  }).catch(err => {
+      console.error("Unable to updating user " + user.id + " . Error JSON:", JSON.stringify(err, null, 2));
+      });        
+}
+
+async function getUserByEmail(email){
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var userTable = env.API_CARDSPACKS_USERTABLE_NAME;
+
+  
+  var userParams = {
+      TableName:userTable,
+      IndexName: "email-index",
+      KeyConditionExpression: "email = :email",
+      ExpressionAttributeValues: {
+          ":email": email
+      }
+  };
+  var user;
+  console.log("searching for user - " + email);
+
+  await docClient.query(userParams).promise().then(data => {
+      console.log("Get user by email succeeded:", JSON.stringify(data, null, 2));
+      if(data["Items"] && data["Items"].length > 0){
+          user = data["Items"][0];
+      }
+  }).catch(err => {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+  });
+
+  return user;
+
+}
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  event.Records.forEach(record => {
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+    let email = record.dynamodb.NewImage.contactEmail.S;
+    let affiliate_id = record.dynamodb.NewImage.affiliateID.S;
+    var user = await getUserByEmail(email);
+    if(user){
+      user.userMyAffiliateId = affiliate_id;
+      user.groupRole = "VIP_USER";
+      saveUser(affiliate_id);
+    }
+  });
+  return Promise.resolve('Successfully processed DynamoDB record');
+};
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -250,6 +250,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "NewAffiliateHandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "storage": {
