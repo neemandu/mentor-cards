efb69b180fa35d766535d039c110b2acf1d81786
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-05-16 06:45:06 +0530

    to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -143,7 +143,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' }),
   ],
   exports: [RouterModule],
 })
--- src/app/main-screen/main-screen.component.spec.ts
+++ src/app/main-screen/main-screen.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MainScreenComponent } from './main-screen.component';
 
@@ -6,7 +6,7 @@ describe('MainScreenComponent', () => {
   let component: MainScreenComponent;
   let fixture: ComponentFixture<MainScreenComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MainScreenComponent ]
     })
