1093263506979906d6453de40b9f1d7198076c56
dudi <neemandu@gmail.com>
2024-05-21 22:00:37 +0300

    aws

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,25 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "PayPalWebhookAPI": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "cardspacks": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
-    },
-    "receiptsAPI": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
   "auth": {
     "mentorcards56dbf153": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "CreatedSNSRole": "string",
+      "GoogleWebClient": "string",
       "HostedUIDomain": "string",
       "OAuthMetadata": "string",
       "UserPoolArn": "string",
@@ -28,18 +13,6 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
-    "AddCardsPack": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "AddCouponCode": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "AddUserToEmailList": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
@@ -54,34 +27,10 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "ChangeCardsPack": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "ContactUsTrigger": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "CreateCommonLink": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "CreateUser": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "DeleteGroup": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
       "Name": "string",
       "Region": "string"
     },
@@ -98,28 +47,10 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "GetEndOfTrialDate": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "GetFreeUntilDate": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "GetNextBillingDate": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "GetNextReceiptId": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
       "Name": "string",
       "Region": "string"
     },
@@ -130,43 +61,7 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "GetSubscriptionPlans": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "IncrementCardPackLike": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "IncrementPackEntries": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "IsCardsPackOwnedByOrgForUser": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "IsInGroup": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "JoinExistingGroup": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "MessagingService": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "Name": "string",
@@ -185,46 +80,10 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "UnSubscribe": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "UpdateGroupUsersList": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "UpdatePaymentProgram": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "UpdateSelectedCardPacks": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "getGroup": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "mentorcards56dbf153PreSignup": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "mentorcards91f3dc29CustomMessage": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
       "Name": "string",
       "Region": "string"
     }
