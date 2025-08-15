944ff192b89b17328179eb7de966128888432e82
IOH4UK <cto-webstick-global>
2025-08-15 22:50:06 +0300

    fix package

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CognitoHostedUIIdentityProvider, Auth } from '@aws-amplify/auth';
-// import { BehaviorSubject } from 'rxjs';
-import { CognitoUser } from 'amazon-cognito-identity-js';
+import { Auth } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 
-import {
-  SocialAuthService,
-  GoogleLoginProvider,
-} from 'angularx-social-login';
 
 export interface AuthState {
   isLoggedIn: boolean;
   username: string | null;
   id: string | null;
   email: string | null;
-  cognitoUser: CognitoUser | null;
+  cognitoUser: SignUpResult | null;
 }
 
 export interface NewUser {
@@ -23,27 +18,31 @@ export interface NewUser {
   password: string,
 };
 
+type SignUpResult = {
+  user: {
+    username: string;
+    attributes: {
+      email: string;
+      phone_number: string;
+      given_name: string;
+      family_name: string;
+    };
+  };
+  userConfirmed: boolean;
+  userSub: string;
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   public loggedIn: boolean;
   public static SIGN_IN = 'signIn';
   public static SIGN_OUT = 'signOut';
   public static FACEBOOK = CognitoHostedUIIdentityProvider.Facebook;
   public static GOOGLE = CognitoHostedUIIdentityProvider.Google;
 
-  constructor(private socialAuthService: SocialAuthService) {
-    // this.socialAuthService.authState.subscribe((user) => {
-    //   console.log(user);
-    // });
-  }
-
-  ngOnInit() {
-  }
-
-  signUp(user: NewUser): Promise<CognitoUser | any> {
+  signUp(user: NewUser): Promise<SignUpResult | any> {
     return Auth.signUp({
       "username": user.email,
       "password": user.password,
@@ -92,21 +91,7 @@ export class AuthService {
     });
   }
 
-  // changePassword(userData: any, oldPassword: string, newPassword: string) {
-  //   return Auth.changePassword(userData, oldPassword, newPassword);
-  // }
-
-  // async signInWithFacebook() {
-  //   const socialResult = await this.socialSignIn(AuthService.FACEBOOK);
-  //   console.log('fb Result:', socialResult);
-  // }
-
   signInWithGoogle() {
-    // this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-    // Auth.federatedSignIn({'provider': AuthService.GOOGLE})
-    // const socialResult = await this.socialSignIn(AuthService.GOOGLE);
-    // console.log('google Result:', socialResult);
     this.socialSignIn(CognitoHostedUIIdentityProvider.Google)
   }
-
 }
